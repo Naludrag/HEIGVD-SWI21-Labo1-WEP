b3b1dd6546a02553a83e04317e03abd597eda1c4
Robin Müller <33422394+AziXus@users.noreply.github.com>
2021-03-11 12:55:02 +0100

    Working on lab

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -17,7 +17,7 @@ from rc4 import RC4
 key= b'\xaa\xaa\xaa\xaa\xaa'
 message= b'\xaa\xaa\x03\x00\x00\x00\x08\x06\x00\x01\x08\x00\x06\x04\x00\x01\x90\x27\xe4\xea\x61\xf2\xc0\xa8\x01\x64\x00\x00\x00\x00\x00\x00\xc0\xa8\x01\xc8'
 ICV = b'\xcc\x88\xcb\xb2'
-IV = b'\x0cM\\'
+IV = b'\x00\x00\x00'
 
 
 # rc4 seed est composé de IV+clé
@@ -31,4 +31,8 @@ print('Ciphertext: ' + ciphertext.hex())
 #lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
 arp = rdpcap('arp.cap')[0]
 
-wrpcap('arp2.cap', ciphertext, append=True)  #
+arp.wepdata = ciphertext
+arp.iv = IV
+arp.icv = int.from_bytes(ICV, byteorder='little')
+
+wrpcap('arp2.cap', arp, append=False)  #
